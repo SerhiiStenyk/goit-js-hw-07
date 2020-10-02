441ecaeae476f686c6447931811b5a0916b82d6d
SerhiiS <sergiystenyk@gmail.com>
2020-10-02 18:06:53 +0200

    fix

--- task6.js
+++ task6.js
@@ -5,7 +5,9 @@ function onValidationFormBlur(event) {
   const data = validationForm.value;
   if (data.length >= validLength) {
     validationForm.classList.add('valid');
+    validationForm.classList.remove('invalid');
   } else {
     validationForm.classList.add('invalid');
+    validationForm.classList.remove('valid');
   }
 }
